fa0d5eb4c794372ce52aa93b2d5853aaa0034471
nmquys <mquy2810@gmail.com>
2025-05-12 13:57:05 +0700

    first commit

---
+++ src/QHTT/AppPanel.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class AppPanel extends JPanel
+{
+    public AppPanel()
+    {
+        this.init();
+
+        this.setTitle("Solve");
+        this.setSize(1200, 900);
+        this.setLocationRelative(null);
+
+    }
+    public static void main(String[] args)
+    {
+    }
+}
